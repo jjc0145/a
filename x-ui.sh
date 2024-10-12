e00d805c5a2a122ed69adc14fd59ba47abd34087
jjc0145 <106815898+jjc0145@users.noreply.github.com>
2024-10-13 02:16:19 +0800

    x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -1,19 +1,44 @@
-﻿#!/bin/bash
-# 一键安装 X-UI 并设置登录端口、账号和密码
+#!/usr/bin/expect
 
-# 安装 X-UI
-bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+# 自动安装 X-UI 并设置登录端口、账号和密码
 
-# 设置登录端口为147
-x-ui setting -port 147
+# 设置变量
+set timeout 300
+set username "jjc0145"
+set password "caonima313"
+set port "147"
 
-# 设置登录账号为 jjc0145，密码为 caonima313
-x-ui setting -username jjc0145 -password caonima313
+# 启动安装脚本并自动确认安装
+spawn bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+
+# 模拟输入 "y" 确认继续安装
+expect "确认是否继续?*"
+send "y\r"
+
+# 输入账户名
+expect "请设置您的账户名*"
+send "$username\r"
+
+# 输入密码
+expect "请设置您的密码*"
+send "$password\r"
+
+# 确认密码
+expect "请确认您的密码*"
+send "$password\r"
+
+# 设置登录端口
+expect "请设置面板访问端口*"
+send "$port\r"
+
+# 结束 expect 脚本
+expect eof
 
 # 重启 X-UI 以应用设置
-x-ui restart
+spawn x-ui restart
+expect eof
 
-echo "X-UI 已安装并配置成功！"
-echo "登录地址: http://your_server_ip:147"
-echo "账号: jjc0145"
-echo "密码: caonima313"
+puts "X-UI 已安装并配置成功！"
+puts "登录地址: http://your_server_ip:$port"
+puts "账号: $username"
+puts "密码: $password"

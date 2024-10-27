a91e966414959468b10f3f1ddbc17aba6abd72a7
jjc0145 <106815898+jjc0145@users.noreply.github.com>
2024-10-27 22:46:27 +0800

    Add files via upload

---
+++ ss15.sh
@@ -0,0 +1,4 @@
+﻿yum -y install wget
+wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh
+chmod 777 install.sh
+bash install.sh --port=313 --user=313 --passwd=313

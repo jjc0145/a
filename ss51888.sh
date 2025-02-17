6cb7e25f74d48be5f505511b3e7d2f71b62db08f
jjc0145 <106815898+jjc0145@users.noreply.github.com>
2025-02-17 22:17:11 +0800

    Add files via upload

---
+++ ss51888.sh
@@ -0,0 +1,17 @@
+﻿apt-get update;
+sudo apt install -y shadowsocks-libev;
+
+/etc/init.d/shadowsocks-libev start;
+systemctl start shadowsocks-libev;   
+
+echo '{
+    "server":["::0", "0.0.0.0"],
+    "mode":"tcp_and_udp",
+    "server_port":51888,
+    "local_port":10810,
+    "password":"996996ZZ",
+    "timeout":86400,
+    "method":"chacha20-ietf-poly1305"
+}' > /etc/shadowsocks-libev/config.json;
+
+systemctl restart shadowsocks-libev;
